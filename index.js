ca61373933bde46629d56ac8a542d2366e8ec353
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-15 17:06:44 +0300

    fix

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ import errorMiddleware from './middleware/ErrorHandler.js'
 const PORT = process.env.PORT || 5000
 
 const app = express()
-// Cross-Origin Resource Sharing
-app.use(cors({origin: [ 'https://www.ruswine-spb.ru', 'http://www.ruswine-spb.ru'], credentials: true}))
+
 // middleware для работы с json
 app.use(express.json())
 // middleware для статики (img, css)
@@ -36,7 +35,7 @@ app.use('/api', router)
 app.use(errorMiddleware)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ruswine-spb.ru');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();

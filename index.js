7dbb7815ca210d7c197056af19934b6ca085b290
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-15 18:38:25 +0300

    fix

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use('/api', router)
 app.use(errorMiddleware)
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '* ');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.ruswine-spb.ru');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();

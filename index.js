67c6b29dcd7a37d93594c63b7060b6050daac342
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-15 15:50:53 +0300

    fix

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.use('/api', router)
 // обработка ошибок
 app.use(errorMiddleware)
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 
 
 const start = async () => {

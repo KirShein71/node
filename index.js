fedf33a171d705327acef4166c74bde33b315607
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-08-16 09:57:51 +0300

    fix

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 // Cross-Origin Resource Sharing
-app.use(cors({origin: 'http://www.ruswine-spb.ru', credentials: true}))
+app.use(cors({origin: 'https://www.ruswine-spb.ru', credentials: true}))
 // middleware для работы с json
 app.use(express.json())
 // middleware для статики (img, css)

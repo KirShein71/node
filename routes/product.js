28271857c5180c6892d081887730362d4ba985cc
KirShein71 <103045855+KirShein71@users.noreply.github.com>
2023-11-06 14:34:03 +0300

    fix

--- routes/product.js
+++ routes/product.js
@@ -48,7 +48,8 @@ router.post(
 // обновить свойство товара
 router.put(
     '/:productId([0-9]+)/property/update/:id([0-9]+)',
-   
+    authMiddleware,
+    adminMiddleware,
     ProductInfoController.update
 )
 // удалить свойство товара
